aa49eb9cd765f8422c964ee1cec824b1e5819cca
Fompe96 <Fompe@hotmail.se>
2022-02-01 10:49:04 +0100

    Added basic css styling to canvas and body, fetched reference to canvas using javascript

---
+++ javascript/breakout.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('gameArea');
+
+const gameArea = canvas.getContext('2d');
+
+gameArea.fillStyle = 'rgb(21, 247, 0)';
+gameArea.fillRect(100, 100, 100, 100); 
+
+
